5fee70186d10a202e2161f8bc31d0b206d21c16c
Anthony Gilbert <anthony.gilbert@effectual.com>
2022-12-09 14:22:01 -0600

    Update: Added logging statements to the script

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 echo 'Installing Homebrew...' && \\
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && \\
 echo 'Homebrew Install Completed' && \\
-
+echo 'Installing iterm2...' && \\
+brew install iterm2 && \\
+echo 'Newest version of iterm2 has been installed...' 
